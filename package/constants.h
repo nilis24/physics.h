8c8416dd374e372257ae8ed0174b303827c62e7e
YashAdake <yashadake91@gmail.com>
2022-10-01 23:06:00 +0530

    Added constants of mass of proton and electron and also added Planks constant

--- package/constants.h
+++ package/constants.h
@@ -2,3 +2,15 @@ double G = 6.67E-11;
 
 //Coulomb constant
 const double k = 8.987*1e9;
+
+//Speed of light in m/s
+const double c = 2.998 * 100000000;
+
+//Mass of proton in Kg
+const double mp = 1.673E-27;
+
+//Mass of electron in Kg
+const double me = 9.110E-31; 
+
+//Planks Constat in Js
+const double k = 6.626E-34;
