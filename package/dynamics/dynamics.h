f2d9480b4f00c96a91766ed84384dc5018293aee
TEJA <saiteja.palegar@gmail.com>
2022-10-01 22:50:22 +0530

    dynamics

---
+++ package/dynamics/dynamics.h
@@ -0,0 +1,17 @@
+void zeroLawofthermo()
+{
+    printf("The zeroth law of thermodynamics defines thermal equilibrium and forms a basis for the definition of temperature: If two systems are each in thermal equilibrium with a third system, then they are in thermal equilibrium with each other.");
+}
+void firstlawofthermo(int a, int b)
+{
+    printf("The first law of thermodynamics is a version of the law of conservation of energy, adapted for thermodynamic processes. In general, the conservation law states that the total energy of an isolated system is constant; energy can be transformed from one form to another, but can be neither created nor destroyed.");
+    printf("Internal Energy = %d" , a - b);
+}
+void secondlawofthermo(){
+    printf('The second law of thermodynamics indicates the irreversibility of natural processes, and in many cases, the tendency of natural processes to lead towards spatial homogeneity of matter and energy, especially of temperature');
+    printf("Entropy = Heat Transferred / Temperature ");
+}
+void thirdlawofthermo()
+{
+    printf("A system's entropy approaches a constant value as its temperature approaches absolute zero.");
+}
