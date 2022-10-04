93d23de17b7da3a8c5c798e2e220bd28b1f15da5
TEJA <saiteja.palegar@gmail.com>
2022-10-04 12:52:28 +0530

    thermodynamics

---
+++ package/thermodynamics/thermodynamics.h
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include "../constants.h"
+
+void zerothlaw()
+{
+    printf("he zeroth law states that if two thermodynamic systems are in thermal equilibrium with each other, and also separately in thermal equilibrium with a third system, then the three systems are in thermal equilibrium with each other.");
+}
+
+void firstlaw()
+{
+    printf("The Change in internal energy is equal to the heat added to the system minus the work done by the system.");
+}
+
+void secoundlaw()
+{
+    printf("Entropy of a isolated system always increases, heat flows from hot body to a cold body.");
+}
+
+void thirdlaw()
+{
+    printf("Entropy of a system approaches a constant as temperature approaches absolute zero.");
+}
